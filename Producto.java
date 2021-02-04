00c500bbdefe75bb2ba770041f9613a8496afe13
rociochc <rhernandez@ids.upchiapas.edu.mx>
2021-02-03 18:50:30 -0600

    Primera version

---
+++ Producto.java
@@ -0,0 +1,28 @@
+public class Producto{
+
+    public Producto(){        }
+
+    private int idProdcuto;
+    private String nombreProducto;
+    private String Marca;
+    private float precio;
+
+    public int getIdProducto(){
+        return idProdcuto;
+    } 
+    public String getNombreProducto(){
+        return nombreProducto;
+    }
+
+    public String getMarca(){
+        return Marca;
+    }
+
+    public float getPrecio(){
+        return precio;
+    }
+
+    public Agregar(){}
+
+    public Eliminar(){}
+}
